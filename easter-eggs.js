61cc38dd2b5516e10fdda0c651da3a4cd1e87527
José Pablo Alpaca Rivera <jose.p.alpaca@gmail.com>
2022-02-11 01:26:44 -0500

    add Loader and skeleton to games

--- easter-eggs.js
+++ easter-eggs.js
@@ -1,37 +1,68 @@
-function TicTacToe() {
+function TicTacToe() {}
+
+function Marquee() {}
+
+function Loader(frames = ["|", "/", "-", "\\"]) {
+  this.frames = frames;
+  let index = 0;
+
+  const animate = () => {
+    if (index >= this.frames.length) index = 0;
+    console.clear();
+    console.log(this.frames[index]);
+    index += 1;
+  };
+
+  this.render = function () {
+    return setInterval(animate, 250);
+  };
+
+  this.load = () => {
+    const interval = this.render();
+    setTimeout(() => {
+      clearInterval(interval);
+      console.clear();
+    }, 3000);
+  };
 }
 
-function Marquee() {
-  
-}
-
-function Loader() {
-}
-
-function Clock() {
-}
+function Clock() {}
 
 function numberFormatter(size, ...args) {
   this.size = size;
   this.collection = [...args][0];
+
   this.play = (number) => {
     let len = this.collection.length;
     for (let i = 0; i < len; i++) {
       if (this.size ** i > number) {
         const position = i - 1;
-        return Math.floor(number / (this.size ** position)) + this.collection[position];
+        return (
+          Math.floor(number / this.size ** position) + this.collection[position]
+        );
       }
     }
-    return Math.floor(number / (this.size ** (len - 1))) + this.collection[len - 1];
+    return (
+      Math.floor(number / this.size ** (len - 1)) + this.collection[len - 1]
+    );
   };
 }
 
 function EasterEgg(game, ...args) {
+  this.loader = new Loader();
+  const interval = this.loader.load();
+  clearInterval(interval);
+
   this.game;
   this.snippet;
+
   const listGames = {
-    "number formatter": numberFormatter 
-  }
+    "clock": Clock,
+    "number formatter": numberFormatter,
+    "tic tac toe": TicTacToe,
+    "marquee": Marquee
+  };
+
   this.game = new listGames[game](...args);
   this.snippet = this.game.play;
 }
